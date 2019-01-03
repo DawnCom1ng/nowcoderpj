07a14494e954df748994404cb4ce085b2fe41ee8
DawnCom1ng <xiangruli1231@gmail.com>
2019-01-03 21:32:33 +0800

    1.3

---
+++ projectnowcoder/src/main/java/com/newcoder/projectnowcoder/configuration/WendaWebConfiguration.java
@@ -0,0 +1,26 @@
+package com.newcoder.projectnowcoder.configuration;
+
+
+import com.newcoder.projectnowcoder.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    PassportInterceptor passportInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor);
+        super.addInterceptors(registry);
+    }
+}
--- projectnowcoder/src/main/java/com/newcoder/projectnowcoder/service/UserService.java
+++ projectnowcoder/src/main/java/com/newcoder/projectnowcoder/service/UserService.java
@@ -1,12 +1,18 @@
 package com.newcoder.projectnowcoder.service;
 
+import com.newcoder.projectnowcoder.dao.LoginTicketDAO;
+
 import com.newcoder.projectnowcoder.dao.UserDAO;
+
+import com.newcoder.projectnowcoder.model.LoginTicket;
 import com.newcoder.projectnowcoder.model.User;
 
+import com.newcoder.projectnowcoder.util.WendaUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 
@@ -18,6 +24,81 @@ public class UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
     private UserDAO userDAO;
+
+    @Autowired
+    private LoginTicketDAO loginTicketDao;
+
+    public Map<String, String> register(String username, String password) {
+        Map<String, String> map = new HashMap<String, String>();
+        if(StringUtils.isEmpty(username)) {
+            map.put("mag", "username can't empty!");
+            return map;
+        }
+
+        if(StringUtils.isEmpty(password)) {
+            map.put("mag", "password can't empty!");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+        if(user != null) {
+            map.put("msg", "username has been register");
+            return map;
+        }
+
+        user = new User();
+        user.setName(username);
+        user.setSalt(UUID.randomUUID().toString().substring(0,5));
+        user.setHeadUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setPassword(WendaUtil.MD5(password+user.getSalt()));
+        userDAO.addUser(user);
+
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+
+        return map;
+    }
+
+    public Map<String, String> login(String username, String password) {
+        Map<String, String> map = new HashMap<String, String>();
+        if(StringUtils.isEmpty(username)) {
+            map.put("msg", "username can't be empty!");
+            return map;
+        }
+
+        if (StringUtils.isEmpty(password)) {
+            map.put("msg", "password can't be empty!");
+            return map;
+        }
+
+        User user = userDAO.selectByName(username);
+        if (user == null) {
+            map.put("msg", "There is no user!");
+            return map;
+        }
+
+        if (!WendaUtil.MD5(password + user.getSalt()).equals(user.getPassword())) {
+            map.put("msg", "password wrong!");
+            return map;
+        }
+
+        String ticket = addLoginTicket(user.getId());
+        map.put("ticket", ticket);
+
+        return map;
+    }
+
+    public String addLoginTicket(int userId){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(userId);
+        Date now = new Date();
+        now.setTime(3600*24*100 + now.getTime());
+        loginTicket.setExpired(now);
+        loginTicket.setStatus(0);
+        loginTicket.setTicket(UUID.randomUUID().toString().replaceAll("-", ""));
+        loginTicketDao.addTicket(loginTicket);
+        return loginTicket.getTicket();
+    }
 
     public User getUser(int id) {
         return userDAO.selectById(id);
